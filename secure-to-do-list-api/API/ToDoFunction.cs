e9ff4925ea1d8d3f60c6081b5c3acf2ead32cd4a
Charl Rabie <84493519+CharlRabieRAC@users.noreply.github.com>
2023-04-26 10:25:10 +0800

    Added LiteBD support and separated function into restfull calls

--- secure-to-do-list-api/API/ToDoFunction.cs
+++ secure-to-do-list-api/API/ToDoFunction.cs
@@ -1,64 +1,69 @@
+using LiteDB;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-using secure_to_do_list_api.Models;
-using secure_to_do_list_api.Queries;
-using System;
-using System.Collections.Generic;
-using System.Net.Mime;
-using System.Net;
-using System.Text.Json;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
+using secure_to_do_list_api.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace secure_to_do_list_api.API
 {
     public static class ToDoFunction
     {
-        [FunctionName("ToDoList")]
-        [ProducesResponseType(typeof(IActionResult), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status408RequestTimeout)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        //[OpenApiOperation("GetPaymentAccounts", Summary = "Gets the member's payment accounts", Visibility = OpenApiVisibilityType.Important)]
-        //[OpenApiResponseWithBody(HttpStatusCode.OK, MediaTypeNames.Application.Json, typeof(PaymentAccountsResponse), Summary = "Payment Accounts")
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        [FunctionName("GetToDoList")]
+        public static async Task<IActionResult> GetToDoList(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
+            using var db = new LiteDatabase(@"MyData.db");
             log.LogInformation("C# HTTP trigger function processed a request.");
-            List<ToDoItem> responseMessage = null;
-            switch (req.Method)
-            {
-                case "GET":
-                    responseMessage = new GetToDoList().Handle("");
-                    break;
+            var todoData = db.GetCollection<ToDoItem>("ToDoItems");
 
-                case "POST":
-                    log.LogInformation("C# HTTP trigger function processed a request.");
-
-                    var item = JsonConvert.DeserializeObject<ToDoItem>(await req.ReadAsStringAsync());
-
-                    responseMessage = new GetToDoList().Handle("");
-                    responseMessage.Add(item);
-
-                    break;
-
-                default:
-                    throw new NotImplementedException();
-                    break;
-            }
-
-            //string name = req.Query["name"];
-
-            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            //dynamic data = JsonConvert.DeserializeObject(requestBody);
-            //name = name ?? data?.name;
+            var item = JsonConvert.DeserializeObject<ToDoItem>(await req.ReadAsStringAsync());
+            IEnumerable<ToDoItem> responseMessage = null;
+            responseMessage = todoData.FindAll().ToList();
 
             return new OkObjectResult(responseMessage);
+        }
+
+        [FunctionName("AddToDoItem")]
+        public static async Task<IActionResult> AddToDoItem(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            using var db = new LiteDatabase(@"MyData.db");
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            var todoData = db.GetCollection<ToDoItem>("ToDoItems");
+
+            var item = JsonConvert.DeserializeObject<ToDoItem>(await req.ReadAsStringAsync());
+            IEnumerable<ToDoItem> responseMessage = null;
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            todoData.EnsureIndex(x => x.Id, true);
+
+            todoData.Insert(item);
+
+            responseMessage = todoData.FindAll().ToList();
+
+            return new OkObjectResult(responseMessage);
+        }
+
+        [FunctionName("DeleteTodoItem")]
+        public static Task<IActionResult> DeleteTodo(
+            [HttpTrigger(AuthorizationLevel.Function, "delete", Route = "DeleteTodoItem/{id}")] HttpRequest req, ILogger log, string id)
+        {
+            using var db = new LiteDatabase(@"MyData.db");
+            IEnumerable<ToDoItem> responseMessage = null;
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            var todoData = db.GetCollection<ToDoItem>("ToDoItems");
+            todoData.DeleteMany(r => r.Id == id);
+            responseMessage = todoData.FindAll().ToList();
+
+            return Task.FromResult<IActionResult>(new OkObjectResult(responseMessage));
         }
     }
 }
